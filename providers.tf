05ad6aeeda8d07988c92aacb17ebc2310a40f535
Tanvir-Alam625 <devtanvir01@gmail.com>
2026-08-08 15:52:07 +0600

    update project tag

--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ provider "aws" {
   default_tags {
     tags = {
         Project     = "tv-lab"
-        Environment = "dev"
+        Environment = "prod"
         ManagedBy   = "Terraform"
         Owner       = "Tanvir"
     }
